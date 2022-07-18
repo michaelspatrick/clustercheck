9c3fa19b95b2e43693e807a5cdb27aa039e8f031
Mike Patrick <45206934+michaelspatrick@users.noreply.github.com>
2022-07-18 15:00:41 -0400

    Update clustercheck.cc

--- src/clustercheck.cc
+++ src/clustercheck.cc
@@ -18,53 +18,17 @@
 #include <typelib.h>
 #include <mysql_com.h>
 
-extern my_bool read_only;
-long long enabled;
-long long available_if_donor;
-long long available_if_readonly;
+extern bool read_only;
 
-int enabled_check(MYSQL_THD thd, struct st_mysql_sys_var *var, void *save, struct st_mysql_value *value) {
-  long long buf;
-  value->val_int(value, &buf);
-  *(long long*) save = buf;
-  return 0;
-}
-void enabled_update(MYSQL_THD thd, struct st_mysql_sys_var *var, void *var_ptr, const void *save) {
-    enabled = *(long long *)save;
-}
-MYSQL_SYSVAR_LONGLONG(enabled, enabled, 0, "Status of clustercheck", enabled_check, enabled_update, 1, 0, 1, 0);
+long enabled = 1;
+long available_if_donor = 0;
+long available_if_readonly = 0;
+static MYSQL_SYSVAR_LONG(available_if_readonly, available_if_readonly, 0, "Availability of node if it is readonly", NULL, NULL, 0, 0, 1, 0);
+static MYSQL_SYSVAR_LONG(available_if_donor, available_if_donor, 0, "Availability of node if it is a donor", NULL, NULL, 0, 0, 1, 0);
+static MYSQL_SYSVAR_LONG(enabled, enabled, 1, "Whether cluster check is enabled", NULL, NULL, 0, 0, 1, 0);
+static SYS_VAR *system_variables[] = {MYSQL_SYSVAR(enabled), MYSQL_SYSVAR(available_if_donor), MYSQL_SYSVAR(available_if_readonly), nullptr};
 
-int available_if_donor_check(MYSQL_THD thd, struct st_mysql_sys_var *var, void *save, struct st_mysql_value *value) {
-  long long buf;
-  value->val_int(value, &buf);
-  *(long long*) save = buf;
-  return 0;
-}
-void available_if_donor_update(MYSQL_THD thd, struct st_mysql_sys_var *var, void *var_ptr, const void *save) {
-    available_if_donor = *(long long *)save;
-}
-MYSQL_SYSVAR_LONGLONG(available_if_donor, available_if_donor, 0, "Availability of node if it is a donor", available_if_donor_check, available_if_donor_update, 1, 0, 1, 0);
-
-int available_if_readonly_check(MYSQL_THD thd, struct st_mysql_sys_var *var, void *save, struct st_mysql_value *value) {
-  long long buf;
-  value->val_int(value, &buf);
-  *(long long*) save = buf;
-  return 0;
-}
-void available_if_readonly_update(MYSQL_THD thd, struct st_mysql_sys_var *var, void *var_ptr, const void *save) {
-    available_if_readonly = *(long long *)save;
-}
-MYSQL_SYSVAR_LONGLONG(available_if_readonly, available_if_readonly, 0, "Availability of node if it is readonly", available_if_readonly_check, available_if_readonly_update, 0, 0, 1, 0);
-
-struct st_mysql_sys_var* vars_enabled[] =
-{
-MYSQL_SYSVAR(enabled),
-MYSQL_SYSVAR(available_if_donor),
-MYSQL_SYSVAR(available_if_readonly),
-NULL
-};
-
-void *listener(void *vargp) {
+void *listener(void *) {
   int listenfd = 0, connfd = 0;
   struct sockaddr_in serv_addr;
   int listen_port = 9200;
@@ -90,11 +54,14 @@ void *listener(void *vargp) {
       strcat(sendBuff, "Content-Length: 51\r\n");
       strcat(sendBuff, "\r\n");
       strcat(sendBuff, "Percona XtraDB Cluster Node is manually disabled.\r\n");
-      write(connfd, sendBuff, strlen(sendBuff));
+      [&]{ return write(connfd, sendBuff, strlen(sendBuff)); }();
+
       close(connfd);
+
     //} else if (wsrep_local_state == "4" || ((wsrep_local_state == "2") && (available_if_donor == 1))) {
     } else if (available_if_donor == 1) {
       if (available_if_readonly == 1) {
+
         if (read_only) {
           // in read-only state
           strcpy(sendBuff, "HTTP/1.1 503 Service Unavailable\r\n");
@@ -103,7 +70,7 @@ void *listener(void *vargp) {
           strcat(sendBuff, "Content-Length: 43\r\n");
           strcat(sendBuff, "\r\n");
           strcat(sendBuff, "Percona XtraDB Cluster Node is read-only.\r\n");
-          write(connfd, sendBuff, strlen(sendBuff));
+          [&]{ return write(connfd, sendBuff, strlen(sendBuff)); }();
           close(connfd);
         } else {
           // success
@@ -113,18 +80,18 @@ void *listener(void *vargp) {
           strcat(sendBuff, "Content-Length: 40\r\n");
           strcat(sendBuff, "\r\n");
           strcat(sendBuff, "Percona XtraDB Cluster Node is synced.\r\n");
-          write(connfd, sendBuff, strlen(sendBuff));
+          [&]{ return write(connfd, sendBuff, strlen(sendBuff)); }();
           close(connfd);
         }
       } else {
-	// success
+        // success
         strcpy(sendBuff, "HTTP/1.1 200 OK\r\n");
         strcat(sendBuff, "Content-Type: text/plain\r\n");
         strcat(sendBuff, "Connection: close\r\n");
         strcat(sendBuff, "Content-Length: 40\r\n");
         strcat(sendBuff, "\r\n");
         strcat(sendBuff, "Percona XtraDB Cluster Node is synced.\r\n");
-        write(connfd, sendBuff, strlen(sendBuff));
+        [&]{ return write(connfd, sendBuff, strlen(sendBuff)); }();
         close(connfd);
       }
     } else {
@@ -135,7 +102,7 @@ void *listener(void *vargp) {
       strcat(sendBuff, "Content-Length: 44\r\n");
       strcat(sendBuff, "\r\n");
       strcat(sendBuff, "Percona XtraDB Cluster Node is not synced.\r\n");
-      write(connfd, sendBuff, strlen(sendBuff));
+      [&]{ return write(connfd, sendBuff, strlen(sendBuff)); }();
       close(connfd);
     }
 
@@ -144,29 +111,19 @@ void *listener(void *vargp) {
   }
 }
 
-struct st_mysql_daemon clustercheck_daemon_plugin =
-{ MYSQL_DAEMON_INTERFACE_VERSION  };
+static struct st_mysql_daemon clustercheck_daemon_plugin = { MYSQL_DAEMON_INTERFACE_VERSION };
 
-//static FILE *fp;
-
-int clustercheck_daemon_plugin_init(void *p)
-{
+static int clustercheck_daemon_plugin_init(void *) {
   pthread_t thread_id;
   pthread_create(&thread_id, NULL, listener, NULL);
-  //pthread_join(thread_id, NULL);
   return 0;
 }
 
-int clustercheck_daemon_plugin_deinit(void *p)
-{
-  //close(connfd);
-  //fprintf(fp, "Shutting down Cluster Check Daemon!!\n");
-  //fclose(fp);
+static int clustercheck_daemon_plugin_deinit(void *) {
   return 0;
 }
 
-mysql_declare_plugin(clustercheck_daemon)
-{
+mysql_declare_plugin(clustercheck_daemon) {
   MYSQL_DAEMON_PLUGIN,
   &clustercheck_daemon_plugin,
   "clustercheck",
@@ -174,10 +131,12 @@ mysql_declare_plugin(clustercheck_daemon)
   "Returns status of the cluster for proxy",
   PLUGIN_LICENSE_GPL,
   clustercheck_daemon_plugin_init, /* Plugin Init */
+  nullptr,
   clustercheck_daemon_plugin_deinit, /* Plugin Deinit */
   0x0100,
   NULL,
-  vars_enabled, /* clustercheck_system_variables, */
-  NULL
+  system_variables, /* clustercheck_system_variables, */
+  NULL,
+  0
 }
 mysql_declare_plugin_end;
